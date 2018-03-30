cd15c2ae387f74fdaac588ac3710636d8af77dbb
KenG <naykeng1kz1@gmail.com>
2018-03-30 08:55:09 +0700

    albums done for first time!!

---
+++ prod/albums/index.android.js
@@ -0,0 +1,10 @@
+// Index.android.js - place code in here for Android!!!!
+
+// Import a library to help create a omponent
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+
+// Render it to the device
---
+++ prod/albums/index.ios.js
@@ -0,0 +1,14 @@
+// Index.ios.js - place code in here for IOS!!!!
+
+// Import a library to help create a omponent
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+	<Text>Some Text</Text>
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
